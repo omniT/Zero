4aac251a411f90d62bbd886d4369942fda67128a
daniel <daniel.hernandez.vicente@gmail.com>
2015-03-19 19:32:11 +0100

    user and node prototypes have been created

--- model/mongoSchema.js
+++ model/mongoSchema.js
@@ -1,2 +1,30 @@
 var mongoose = require('mongoose'),
-	db = mongoose.createConnection('mongodb://localhost:27017/zero');
+	mongoose.connect('mongodb://localhost/zero');
+	
+/*
+	User model
+*/
+var userSchema = mongoose.Schema({
+    name : String,
+    password : String
+
+});	
+
+/*
+	Node model
+*/
+var  nodeSchema = mongoose.Schema({
+	name : String,
+    token : String
+
+});
+
+// prototipes to use
+var user = db.model('user', userSchema);
+var node = db.model('node', nodeSchema);
+
+
+
+
+
+
