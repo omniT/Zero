ce819a55125e2c6b45302b44d4132a18446b4d3c
daniel <daniel.hernandez.vicente@gmail.com>
2015-04-21 02:17:29 +0200

    process to generate properties.js symbolic link simplified, useless libraries deleted from package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,14 @@
 		make:
 			-create a simbolic link from properties.js to node_modules
 */			
+var fs = require('fs');
 
 module.exports = function(grunt) {
-    // grunt config:
-    grunt.initConfig({
-    	shell: {
-        	propertieslink: {
-            	command: 'ln -s ' + __dirname + '/properties.js ' + __dirname + '/node_modules/properties.js',
-        	}
-    	}
+
+	//Grunt task to set build the basic project configuration:
+	grunt.registerTask('build', 'My "default" task description.', function() {
+		//Create a symbolic link to set properties.js file as a general library:
+  		fs.symlinkSync(__dirname + '/properties.js', __dirname + '/node_modules/properties.js');
 	});
-
-	//Import required libraries
-	grunt.loadNpmTasks('grunt-shell');
-
-	//Rergister new task:
-	grunt.registerTask('make', ['shell']);
 };
 
